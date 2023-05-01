3bbe9db5907305f0954f4dc0d3d317304cd95ff9
SkrytyZubr <michal.czarnecki03@gmail.com>
2023-05-01 19:12:41 +0200

    fixed .env file (postgresql database settings)

--- app/website/website/env.bat
+++
@@ -1,5 +0,0 @@
-SET DB_NAME=dronexdb
-SET DB_USER=postgres
-SET DB_USER_PASSWORD=adminadmin
-SET DB_HOST=localhost
-SET DB_PORT=5432
--- app/website/website/settings.py
+++ app/website/website/settings.py
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'website.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dronexdb',
-        'USER': 'postgres',
-        'PASSWORD': 'adminadmin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'NAME': 'dronexdb',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'adminadmin',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
 
 
-        # 'NAME': os.environ.get('DB_NAME'),
-        # 'USER': os.environ.get('DB_USER'),
-        # 'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
-        # 'HOST': os.environ.get('DB_HOST'),
-        # 'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
